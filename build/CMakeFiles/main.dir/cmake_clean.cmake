948215433dd93099d566492dafd7271ebfbacf47
GustavoZPretto <gustavopretto04@gmail.com>
2024-12-11 19:07:24 -0300

    Usando arquivos do Laboratorio 4 como base do trabalho.

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/gustavo/ufrgs/fcg/trabalho-fcg/bin/Linux/main"
+  "/home/gustavo/ufrgs/fcg/trabalho-fcg/bin/Linux/main.pdb"
+  "CMakeFiles/main.dir/src/glad.c.o"
+  "CMakeFiles/main.dir/src/glad.c.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.o"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
